6e668feeb50bfd29621b77b91a435582de873e85
tomvalriseplayer <nemo.valrise@outlook.com>
2024-12-19 21:30:25 +0200

    fix

--- Javascript/dashboard.js
+++ Javascript/dashboard.js
@@ -1,7 +1,7 @@
 const data = {
     manual: [
         {
-            title: 'VOLUME I | ORGANIZATION AND FUNCTIONS',
+            title: 'VOLUME IE | ORGANIZATION AND FUNCTIONS',
             details: 'Media/Structure.png',
             icon: '<i class="fa-solid fa-sitemap"></i>', // Officer icon
             cssPath: '' // No CSS needed for images
