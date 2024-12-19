4b2f8e664d6209a1517479d2724e50e3180aaaea
tomvalriseplayer <nemo.valrise@outlook.com>
2024-12-19 11:16:18 +0200

    Test

--- Javascript/dashboard.js
+++ Javascript/dashboard.js
@@ -1,7 +1,7 @@
 const data = {
     officers: [
         {
-            title: 'VOLUME I | ORGANIZATION AND FUNCTIONS',
+            title: 'VOLUME I... | ORGANIZATION AND FUNCTIONS',
             details: 'Media/Structure.png',
             icon: '<i class="fa-solid fa-sitemap"></i>' // Officer icon
         },
